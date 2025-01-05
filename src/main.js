8fe681ec6b1d2bb18843e202444641f884f18d14
max <mwang98@hawk.iit.edu>
2025-01-05 10:39:12 +0800

    Branch main.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'; // Import createApp from Vue 3
 import App from './App.vue'; // Import the root component
-import 'bootstrap'; // Import Bootstrap JS
+import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 
 const app = createApp(App); // Create the Vue app with the App component
